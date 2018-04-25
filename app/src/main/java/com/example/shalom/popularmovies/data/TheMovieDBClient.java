4e52fad22c10ade8e28e6b41a5e7ff69fdfb8b32
Shalom Halbert <shalomhalbert@gmail.com>
2018-04-25 17:17:35 -0400

    Added dynamic path for movie details; refactored Call generic name

--- app/src/main/java/com/example/shalom/popularmovies/data/TheMovieDBClient.java
+++ app/src/main/java/com/example/shalom/popularmovies/data/TheMovieDBClient.java
@@ -1,6 +1,7 @@
 package com.example.shalom.popularmovies.data;
 
-import com.example.shalom.popularmovies.data.model.Result;
+import com.example.shalom.popularmovies.data.model.MovieListResult;
+import com.example.shalom.popularmovies.data.model.VideoResults;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -12,11 +13,10 @@ import retrofit2.http.Query;
  */
 
 public interface TheMovieDBClient {
-
-//    @GET("movie/top_rated?api_key={api_key}")
-//    Call<Result> topRatedMovies(@Path("api_key") String api_key);
+    @GET("movie/{movie_id}/videos")
+    Call<VideoResults> videos(@Path("movie_id") String movieID, @Query("api_key") String apiKey);
 
     @GET("movie/{order_by}")
-    Call<Result> popularMovies(@Path("order_by") String orderBy, @Query("api_key") String api_key, @Query("page") int page);
+    Call<MovieListResult> popularMovies(@Path("order_by") String orderBy, @Query("api_key") String apiKey, @Query("page") int page);
 
 }
