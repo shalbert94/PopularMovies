729ac794f67c71b74fb97d3489ffde5256c54468
Shalom Halbert <shalomhalbert@gmail.com>
2018-05-04 19:05:29 -0400

    Moved OnClicks to one method; Added method for trailer and reviews
    - Effected by Model name refactoring
    - Repositioned api key
    - Moved OnClickListener initializations to single method
    - Added method for starting implicit intent for watching a Trailer
    - Added webservice for getting list of movie reviews

--- app/src/main/java/com/example/shalom/popularmovies/MovieDetailsFragment.java
+++ app/src/main/java/com/example/shalom/popularmovies/MovieDetailsFragment.java
@@ -16,14 +16,20 @@ import android.view.ViewGroup;
 import com.example.shalom.popularmovies.data.ServiceGenerator;
 import com.example.shalom.popularmovies.data.TheMovieDBClient;
 import com.example.shalom.popularmovies.data.model.Movie;
-import com.example.shalom.popularmovies.data.model.Video;
-import com.example.shalom.popularmovies.data.model.VideoResults;
+import com.example.shalom.popularmovies.data.model.Review;
+import com.example.shalom.popularmovies.data.model.Reviews;
+import com.example.shalom.popularmovies.data.model.Trailers;
+import com.example.shalom.popularmovies.data.model.Trailer;
 import com.example.shalom.popularmovies.databinding.FragmentMovieDetailsBinding;
 import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
 
-import butterknife.OnClick;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Request;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -33,18 +39,24 @@ import retrofit2.Response;
  * A simple {@link Fragment} subclass.
  * Fragment controlling screen with movie details
  */
-public class MovieDetailsFragment extends Fragment {
 
+//TODO(1) Add onClick's for review buttons
+public class MovieDetailsFragment extends Fragment {
     public static final String LOG_TAG = MovieDetailsFragment.class.getSimpleName();
 
     /*Key used to persist data through the received {@code Bundle}*/
     public static final String MOVIE_KEY = "MOVIE_KEY";
+
+    private String api_key;
 
     private Movie thisMovie;
     /*Data binder*/
     private FragmentMovieDetailsBinding binding;
 
     private TheMovieDBClient theMovieDBClient;
+
+    private List<Review> reviews;
+    private int reviewsIndex = 0;
 
     public MovieDetailsFragment() {
         // Required empty public constructor
@@ -57,6 +69,8 @@ public class MovieDetailsFragment extends Fragment {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_movie_details, container, false);
         /*Inflate the layout for this fragment*/
         View view = binding.getRoot();
+        /*Api key for movie db*/
+        api_key = getResources().getString(R.string.the_movie_db_api_key);
 
         theMovieDBClient = ServiceGenerator.createService(TheMovieDBClient.class);
 
@@ -80,34 +94,9 @@ public class MovieDetailsFragment extends Fragment {
         String url = "https://image.tmdb.org/t/p/w500" + posterPath;
         Picasso.get().load(url).into(binding.detailsPosterImageview);
 
-        binding.playTrailerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.e(LOG_TAG, "onClick() called");
+        setOnClickListers();
+        getReviews();
 
-                final String api_key = getResources().getString(R.string.the_movie_db_api_key);
-                theMovieDBClient.videos(thisMovie.getId().toString(), api_key).enqueue(new Callback<VideoResults>() {
-                    @Override
-                    public void onResponse(Call<VideoResults> call, Response<VideoResults> response) {
-                        if (response.isSuccessful()) {
-                            Log.d(LOG_TAG, "videos() request sucessfull");
-                            for (Video video : response.body().getVideos()) {
-                                /*Send intent to youtube for a YouTube video that's a movie trailer*/
-                                if (video.getSite().equals("YouTube") && video.getType().equals("Trailer")) {
-                                    watchYoutubeVideo(getContext(), video.getKey());
-                                    break;
-                                }
-                            }
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<VideoResults> call, Throwable t) {
-
-                    }
-                });
-            }
-        });
 
         return view;
     }
@@ -123,5 +112,97 @@ public class MovieDetailsFragment extends Fragment {
             context.startActivity(webIntent);
         }
     }
+
+    private void setOnClickListers() {
+        binding.playTrailerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e(LOG_TAG, "onClick() called");
+
+                theMovieDBClient.videos(thisMovie.getId().toString(), api_key).enqueue(new Callback<Trailers>() {
+                    @Override
+                    public void onResponse(Call<Trailers> call, Response<Trailers> response) {
+                        if (response.isSuccessful()) {
+                            Log.d(LOG_TAG, "videos() request sucessfull");
+                            for (Trailer trailer : response.body().getTrailers()) {
+                                /*Send intent to youtube for a YouTube trailer that's a movie trailer*/
+                                if (trailer.getSite().equals("YouTube") && trailer.getType().equals("Trailer")) {
+                                    watchYoutubeVideo(getContext(), trailer.getKey());
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Trailers> call, Throwable t) {
+
+                    }
+                });
+            }
+        });
+
+        binding.previousReviewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (reviewsIndex == 0) {
+                    reviewsIndex = reviews.size() - 1;
+                } else {
+                    reviewsIndex--;
+                }
+                String author = reviews.get(reviewsIndex).getAuthor();
+                binding.reviewAuthorTextview.setText("Review by " + author);
+
+            }
+        });
+
+        binding.nextReviewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (reviewsIndex == reviews.size() - 1) {
+                    reviewsIndex = 0;
+                } else {
+                    reviewsIndex++;
+                }
+                String author = reviews.get(reviewsIndex).getAuthor();
+                binding.reviewAuthorTextview.setText("Review by " + author);
+
+            }
+        });
+
+        binding.reviewAuthorTextview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String reviewUrl = reviews.get(reviewsIndex).getUrl();
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(android.net.Uri.parse(reviewUrl));
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void getReviews() {
+        theMovieDBClient.reviews(thisMovie.getId().toString(), api_key).enqueue(new Callback<Reviews>() {
+            /*Get Reviews result*/
+            @Override
+            public void onResponse(Call<Reviews> call, Response<Reviews> response) {
+                if (response.isSuccessful()) {
+                    Log.d(LOG_TAG, "Review request successful");
+                    reviews = response.body().getReviews();
+
+                    String author = reviews.get(reviewsIndex).getAuthor();
+                    binding.reviewAuthorTextview.setText("Review by " + author);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Reviews> call, Throwable t) {
+
+            }
+        });
+
+    }
 }
+
 
